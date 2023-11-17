793dae423f8b183a1e126ce9ff43246c797d3211
wade <elvis122545735@gmail.com>
2023-11-17 23:43:05 +0000

    refactor:'因應前端需求,將表單驗證錯誤返回,均已error作為key來表示;將UserController重複驗證的地方刪掉'

--- config/services.php
+++ config/services.php
@@ -39,8 +39,8 @@ return [
 
     'frontend' => [
         'url' => env('FRONTEND_URL'),
-        'login_url'=> env('FRONTEND_LOGIN_URL'),
-        
+        'login_url' => env('FRONTEND_LOGIN_URL'),
+
     ],
     
 
